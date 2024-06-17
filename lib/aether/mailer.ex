376f18116c59180f85474832e2ca7975da824a8f
Meadow <meadowingc@gmail.com>
2024-06-17 17:30:16 +0000

    initial commit

---
+++ lib/aether/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Aether.Mailer do
+  use Swoosh.Mailer, otp_app: :aether
+end
---
+++ lib/aether_web/controllers/page_html.ex
@@ -0,0 +1,10 @@
+defmodule AetherWeb.PageHTML do
+  @moduledoc """
+  This module contains pages rendered by PageController.
+
+  See the `page_html` directory for all templates available.
+  """
+  use AetherWeb, :html
+
+  embed_templates "page_html/*"
+end
